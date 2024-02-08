86b68a7bb851ac772abb9ddc94a43cf265a9a098
Thomas Hunt <albertderr9978@gmail.com>
2024-02-08 11:16:19 +0930

    Update TradeEntities.sol to the latest version

--- contracts/TradeEntities.sol
+++ contracts/TradeEntities.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Mint.sol";
 
