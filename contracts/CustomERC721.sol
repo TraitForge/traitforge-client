a1a43310a8257ad7dae65b8fd069ad579b955a78
Thomas Hunt <albertderr9978@gmail.com>
2024-02-29 13:34:37 +0930

    changed contracts

--- contracts/CustomERC721.sol
+++ contracts/CustomERC721.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./EntropyGenerator.sol";
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,38 +1,12 @@
-const { ethers } = require("hardhat");
-
 async function main() {
   const [deployer] = await ethers.getSigners();
 
-  const TokenPoolFactory = await ethers.getContractFactory("TokenPool");
-  const tokenPool = await TokenPoolFactory.deploy(); 
-  await tokenPool.deployed();
-  console.log("TokenPool deployed to:", tokenPool.address);
+  console.log("Deploying contracts with the account:", deployer.address);
 
-  const NukeFundFactory = await ethers.getContractFactory("NukeFund");
-  const nukeFund = await NukeFundFactory.deploy(deployer.address);
-  await nukeFund.deployed();
-  console.log("NukeFund deployed to:", nukeFund.address);
+  const EntropyGenerator = await ethers.getContractFactory("EntropyGenerator");
+  const entropyGenerator = await EntropyGenerator.deploy();
 
-  const BreedableTokenFactory = await ethers.getContractFactory("BreedableToken");
-  const breedableToken = await BreedableTokenFactory.deploy(
-    "BreedableTokenName", 
-    "BT",                
-    tokenPool.address,   
-    deployer.address,  
-    ethers.utils.parseEther("0.01") 
-  );
-  await breedableToken.deployed();
-  console.log("BreedableToken deployed to:", breedableToken.address);
-
-  const MintFactory = await ethers.getContractFactory("Mint");
-  const mint = await MintFactory.deploy(tokenPool.address, deployer.address); 
-  await mint.deployed();
-  console.log("Mint deployed to:", mint.address);
-
-  const EntityTradingFactory = await ethers.getContractFactory("EntityTrading");
-  const entityTrading = await EntityTradingFactory.deploy(mint.address, deployer.address, deployer.address);
-  await entityTrading.deployed();
-  console.log("EntityTrading deployed to:", entityTrading.address);
+  console.log("EntropyGenerator address:", entropyGenerator.address);
 }
 
 main()
