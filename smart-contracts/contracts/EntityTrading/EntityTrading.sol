a7a35665b76e9fccecb860f8ac406cc26d26de1d
TForge1 <TraitForge@protonmail.com>
2024-05-23 15:15:35 +0930

    removed contract hardcoded params

--- smart-contracts/contracts/EntityTrading/EntityTrading.sol
+++ smart-contracts/contracts/EntityTrading/EntityTrading.sol
@@ -10,6 +10,7 @@ import '../TraitForgeNft/ITraitForgeNft.sol';
 contract EntityTrading is IEntityTrading, ReentrancyGuard, Ownable {
   ITraitForgeNft public nftContract;
   address payable public nukeFundAddress;
+  uint256 public taxCut = 10;
 
   mapping(uint256 => Listing) public listings;
 
@@ -53,7 +54,7 @@ contract EntityTrading is IEntityTrading, ReentrancyGuard, Ownable {
     require(listing.seller != address(0), 'NFT is not listed for sale.');
 
     //transfer eth to seller (distribute to nukefund)
-    uint256 nukeFundContribution = msg.value / 10;
+    uint256 nukeFundContribution = msg.value / taxCut;
     uint256 sellerProceeds = msg.value - nukeFundContribution;
     transferToNukeFund(nukeFundContribution); // transfer contribution to nukeFund
 
