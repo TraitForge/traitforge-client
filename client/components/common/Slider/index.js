cf08c35cee1e78394ef8934dfc38e1335a37fcf2
TForge1 <TraitForge@protonmail.com>
2024-05-26 10:39:58 +0930

    changed mint prices

--- client/components/common/Slider/index.js
+++ client/components/common/Slider/index.js
@@ -14,8 +14,8 @@ const Slider = () => {
 
   const getEntityPrice = (entityPrice, index) => {
     const numericPrice = Number(entityPrice);
-    const increment = index * 0.01;
-    const calculatedPrice = (numericPrice + increment).toFixed(2);
+    const increment = index * 0.0001;
+    const calculatedPrice = (numericPrice + increment).toFixed(4);
     return calculatedPrice;
   };
 
