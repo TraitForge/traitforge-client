917c64fa7ff5752a0aa1c9aae8da02e1df0f4a99
Thomas Hunt <albertderr9978@gmail.com>
2024-05-29 14:44:44 +0930

    updated front

--- client/components/common/EntityCard/index.js
+++ client/components/common/EntityCard/index.js
@@ -1,77 +1,80 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
+import { ethers } from 'ethers';
 import classNames from 'classnames';
 import orangeBorder from '@/public/images/orangeborder.png';
 import blueBorder from '@/public/images/border.svg';
 import purpleBorder from '@/public/images/purpleBorder.svg';
 import greenBorder from '@/public/images/greenBorder.svg';
-import { useWeb3ModalProvider } from '@web3modal/ethers/react';
 import styles from './styles.module.scss';
 
 export const EntityCard = ({
-  entity,
-  onSelect,
-  borderType = 'blue',
-  wrapperClass,
-  showPrice,
+  entity,
+  onSelect,
+  borderType = 'blue',
+  wrapperClass,
+  showPrice
 }) => {
 
-  const { paddedEntropy, generation, role, forgePotential, performanceFactor, nukeFactor, price} = entity;
 
-    const calculateUri = (paddedEntropy, generation) => {
-      return `${paddedEntropy}_${generation}`;
-    };
+  const { paddedEntropy, generation, role, forgePotential, performanceFactor, nukeFactor, price, fee} = entity;
+  
+  const displayPrice = price || (fee ? ethers.formatEther(fee) : null);
 
-   const uri = calculateUri(paddedEntropy, generation);
+    const calculateUri = (paddedEntropy, generation) => {
+      return `${paddedEntropy}_${generation}`;
+    };
 
-  let activeBorder;
-  switch (borderType) {
-    case 'orange':
-      activeBorder = orangeBorder;
-      break;
-    case 'purple':
-      activeBorder = purpleBorder;
-      break;
-    case 'green':
-      activeBorder = greenBorder;
-      break;
-    default:
-      activeBorder = blueBorder;
-  }
+   const uri = calculateUri(paddedEntropy, generation);
 
-  const wrapperClasses = classNames('mx-5', styles.cardContainer, wrapperClass);
+  let activeBorder;
+  switch (borderType) {
+    case 'orange':
+      activeBorder = orangeBorder;
+      break;
+    case 'purple':
+      activeBorder = purpleBorder;
+      break;
+    case 'green':
+      activeBorder = greenBorder;
+      break;
+    default:
+      activeBorder = blueBorder;
+  }
 
-  return (
-    <div
-      onClick={onSelect}
-      className={`${wrapperClasses} overflow-hidden items-center`}
-      style={{
-        backgroundImage: `url("${activeBorder.src}")`,
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'contain',
-      }}
-    >
-      <div className="w-11/12 mb-4 h-full 3xl:px-10 3xl:pt-10">
-        <Image
-          loading="lazy"
-          src={`https://traitforge.s3.ap-southeast-2.amazonaws.com/${uri}.jpeg`}
-          alt="IMG"
-          className="z-1"
-          width={250}
-          height={350}
-        />
-      </div>
-      <div className="mt-5 mb-5 h-full text-center text-sm md:text-[18px]">
-        <div className={styles.cardInfo}>
-        <h1 className="card-name"> GEN{generation}</h1>
-          {showPrice && <h4 className="">{price} ETH</h4>}
-        </div>
-        {role && <h4 className="card-name">{role}</h4>}
-        <h4 className="card-name">Forge Potential: {forgePotential}</h4>
-        <h4 className="card-name">Nuke Factor: {nukeFactor} %</h4>
-        <h4 className="card-name">Performance Factor: {performanceFactor}</h4>
-      </div>
-    </div>
-  );
+  const wrapperClasses = classNames('mx-5', styles.cardContainer, wrapperClass);
+
+  return (
+    <div
+      onClick={onSelect}
+      className={`${wrapperClasses} overflow-hidden items-center`}
+      style={{
+        backgroundImage: `url("${activeBorder.src}")`,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'contain',
+      }}
+    >
+      <div className="w-11/12 mb-4 h-full 3xl:px-10 3xl:pt-10">
+        <Image
+          loading="lazy"
+          src={`https://traitforge.s3.ap-southeast-2.amazonaws.com/${uri}.jpeg`}
+          alt="IMG"
+          className="z-1"
+          width={250}
+          height={350}
+        />
+      </div>
+      <div className="mt-5 mb-5 h-full text-center text-sm md:text-[18px]">
+        <div className={styles.cardInfo}>
+        <h1 className="card-name"> GEN{generation}</h1>
+          {showPrice && <h4 className="">{displayPrice} ETH</h4>}
+        </div>
+        {role && <h4 className="card-name">{role}</h4>}
+        <h4 className="card-name">Forge Potential: {forgePotential}</h4>
+        <h4 className="card-name">Nuke Factor: {nukeFactor} %</h4>
+        <h4 className="card-name">Performance Factor: {performanceFactor}</h4>
+      </div>
+    </div>
+  );
 };
--- client/utils/utils.js
+++ client/utils/utils.js
@@ -34,12 +34,8 @@ export const getEntitiesHook = async infuraProvider => {
     infuraProvider
   );
   const listings = await contract.fetchListings();
-  const listedEntities = listings.map(listing => ({
-    tokenId: listing.tokenId,
-    isListed: listing.isListed,
-    fee: ethers.formatEther(listing.fee),
-  }));
-  return listedEntities;
+  console.log("forging listings are:", listings)
+  return listings;
 };
 
 export const getUpcomingMintsHook = async (
