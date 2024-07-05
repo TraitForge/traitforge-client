77ef48689478cd633c6367ab5d8836b3ac323ab6
atroboj <atroboj@gmail.com>
2024-07-05 20:19:06 +0800

    feat: update forging from new deployment

--- constants/address.ts
+++ constants/address.ts
@@ -1,12 +1,12 @@
 export const CONTRACT_ADDRESSES = {
-  Trait: '0x47eA9b10E643A70B8BCc5416Cb59d27E125a97e7' as `0x${string}`,
-  TraitForgeNft: '0xd9eD1d414A7354a96E42e16B278310AC59aEcb4D' as `0x${string}`,
+  Trait: '0xa1567eBE9dc8170Def52895f420bBa64E0C64A64' as `0x${string}`,
+  TraitForgeNft: '0x15D18ED9d77aB50AdDb0156540f4a9f55E73E013' as `0x${string}`,
   EntropyGenerator:
-    '0x4b9d22f086F29d591651eEdB7aEC7fA9874ff3a2' as `0x${string}`,
-  EntityTrading: '0xD98fB1c80432c5EF28C664641A41377403bED9dC' as `0x${string}`,
-  EntityForging: '0xDf808173833bB17fDd7bd66aE9afDeE6916f7807' as `0x${string}`,
-  DevFund: '0xd5236DD308C5a9986a012e9D398D7C6f8E2c8BB7' as `0x${string}`,
-  Airdrop: '0xF88E1F794923744FFe3456B2439D07288Cb477db' as `0x${string}`,
-  DAOFund: '0x40c5401Eb27a1C99776fdd7e6b8B1A22E2b3C651' as `0x${string}`,
-  NukeFund: '0xf6289f2c0323a91cD8eb6aADd07E00B2ED9C548a' as `0x${string}`,
+    '0xA1273948E7F0b5e28E2e1fa01d61927f1a93d0aF' as `0x${string}`,
+  EntityTrading: '0x4Ca6f630171194b4a6974B057F18152c25eE3ead' as `0x${string}`,
+  EntityForging: '0x8C89d1F7cDe6AC6D708EC2cF0F2b539aAB0dd49e' as `0x${string}`,
+  DevFund: '0x03D50b74Ec12386c47A904D0526489994f003036' as `0x${string}`,
+  Airdrop: '0xE8368156A698A1a481cCeA29886f74848b9D57A9' as `0x${string}`,
+  DAOFund: '0x94757fF3d68CE96Ee0A586E04243D3CD6e169cc2' as `0x${string}`,
+  NukeFund: '0x493796EC8b6BB7b480F38557519C6F343B988B03' as `0x${string}`,
 };
