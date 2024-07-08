81966a011059e06bfa7a079807016a55b8a8c240
atroboj <atroboj@gmail.com>
2024-07-08 10:29:42 +0800

    feat: add unlisting functions

--- components/screens/WalletModal/index.tsx
+++ components/screens/WalletModal/index.tsx
@@ -1,16 +1,19 @@
 'use client';
 
-import Image from 'next/image';
 import { useAccount } from 'wagmi';
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
 import { FaWallet } from 'react-icons/fa';
 import classNames from 'classnames';
 import { useRouter } from 'next/router';
-import { Button, EntityCard, Modal } from '~/components';
-import { BorderType, Entity } from '~/types';
+import { Button, EntityCard, LoadingSpinner, Modal } from '~/components';
+import { BorderType, Entity, EntityForging, EntityTrading } from '~/types';
 import { shortenAddress } from '~/utils';
-import { useListedEntitiesByUser, useOwnerEntities } from '~/hooks';
+import {
+  useListedEntitiesByUser,
+  useOwnerEntities,
+  useUnlistEntityForForging,
+  useUnlistEntityForSale,
+} from '~/hooks';
 
 type WalletModalTypes = {
   isOpen: boolean;
@@ -25,21 +28,28 @@ export const WalletModal = ({
 }: WalletModalTypes) => {
   const { asPath } = useRouter();
   const { address } = useAccount();
-  const { data: ownerEntities } = useOwnerEntities(address || '0x0');
-  const { data: entitiesListedByUser } = useListedEntitiesByUser(
-    address || '0x0'
-  );
+  const { data: ownerEntities, refetch: refetchOwnerEntities } =
+    useOwnerEntities(address || '0x0');
+  const { data: entitiesListedByUser, refetch: refetchListedEntities } =
+    useListedEntitiesByUser(address || '0x0');
+  const {
+    onWriteAsync: onUnlistTrading,
+    isPending: isUnlistTradingPending,
+    isConfirmed: isUnlistTradingConfirmed,
+  } = useUnlistEntityForSale();
+  const {
+    onWriteAsync: onUnlistForging,
+    isPending: isUnlistForgingPending,
+    isConfirmed: isUnlistForgingConfirmed,
+  } = useUnlistEntityForForging();
 
-  const [selectedForUnlisting, setSelectedForUnlisting] = useState<Entity | null>(null);
+  const isLoading = isUnlistTradingPending || isUnlistForgingPending;
+  const [selectedForUnlisting, setSelectedForUnlisting] = useState<
+    EntityTrading | EntityForging | null
+  >(null);
   const [currentStep, setCurrentStep] = useState(1);
 
   const shortAddress = address ? shortenAddress(address) : 'Not connected';
-
-  const handleSelectEntity = (tokenId: number) => {
-    const entity = entitiesListedByUser.find(e => e.tokenId === tokenId) || null;
-    setSelectedForUnlisting(entity);
-    setCurrentStep(3); // Move to step 3
-  };
 
   const containerClasses = classNames(
     'bg-dark-81 flex justify-center items-center border-[5px] rounded-xl max-w-[90%]',
@@ -52,7 +62,7 @@ export const WalletModal = ({
     }
   );
 
-  let borderType:BorderType;
+  let borderType: BorderType;
   let buttonColor;
 
   switch (asPath) {
@@ -72,6 +82,34 @@ export const WalletModal = ({
       borderType = BorderType.BLUE;
       buttonColor = '#0ADFDB';
   }
+
+  const handleSelectEntity = (tokenId: number) => {
+    const entity =
+      entitiesListedByUser.find(e => e.tokenId === tokenId) ?? null;
+    setSelectedForUnlisting(entity);
+    setCurrentStep(3); // Move to step 3
+  };
+
+  const handleUnlist = () => {
+    if (!selectedForUnlisting) {
+      return;
+    }
+    if ((selectedForUnlisting as EntityTrading).seller) {
+      // EntityTrading
+      onUnlistTrading(selectedForUnlisting.tokenId);
+    } else {
+      onUnlistForging(selectedForUnlisting.tokenId);
+    }
+  };
+
+  useEffect(() => {
+    if (isUnlistTradingConfirmed || isUnlistForgingConfirmed) {
+      setSelectedForUnlisting(null);
+      setCurrentStep(1);
+      refetchListedEntities();
+      refetchOwnerEntities();
+    }
+  }, [isUnlistTradingConfirmed, isUnlistForgingConfirmed]);
 
   return (
     <Modal
@@ -116,7 +154,9 @@ export const WalletModal = ({
               </svg>
               <div>
                 <p className="text-neutral-100 text-sm sm:text-base">ETH</p>
-                <span className="text-white text-md sm:text-large">{balanceInETH}</span>
+                <span className="text-white text-md sm:text-large">
+                  {balanceInETH}
+                </span>
               </div>
             </div>
             <div className="flex items-center gap-x-2.5">
@@ -124,8 +164,12 @@ export const WalletModal = ({
                 <FaWallet color="#0EEB81" />
               </span>
               <div>
-                <p className="text-neutral-100 text-sm sm:text-base">Wallet Address</p>
-                <span className="text-white text-md sm:text-large">{shortAddress}</span>
+                <p className="text-neutral-100 text-sm sm:text-base">
+                  Wallet Address
+                </p>
+                <span className="text-white text-md sm:text-large">
+                  {shortAddress}
+                </span>
               </div>
             </div>
           </div>
@@ -153,15 +197,17 @@ export const WalletModal = ({
       )}
       {currentStep === 2 && (
         <div className="flex items-center flex-col justify-center h-[80vh]">
-          <h3 className="pt-10 text-[18px] md:text-[36px] pb-3">Select an entity to unlist</h3>
+          <h3 className="pt-10 text-[18px] md:text-[36px] pb-3">
+            Select an entity to unlist
+          </h3>
           <div className="pb-5 w-12/12 pt-10 grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 md:text-large text-white md:mb-8 gap-4 px-10 flex-1 overflow-y-scroll">
             {entitiesListedByUser.map((entity: Entity) => (
-           <EntityCard
-              key={entity.tokenId}
-              entity={entity}
-              onSelect={() => handleSelectEntity(entity.tokenId)}
-              borderType={borderType}
-           />
+              <EntityCard
+                key={entity.tokenId}
+                entity={entity}
+                onSelect={() => handleSelectEntity(entity.tokenId)}
+                borderType={borderType}
+              />
             ))}
           </div>
           <div className="w-2/12 flex flex-col justify-center gap-3 pb-5">
@@ -176,27 +222,36 @@ export const WalletModal = ({
       )}
       {currentStep === 3 && selectedForUnlisting && (
         <div className="w-full justify-center flex flex-col h-[80vh] pt-3 md:px-[100px] rounded-[20px] items-center">
-          <div className="mt-3 mb-4 w-7/12 px-2 md:w-6/12 lg:w-4/12 xl:w-3/12 xl:px-4">
+          <div className="mt-3 mb-4 px-2 xl:px-4">
             <EntityCard
-               entity={selectedForUnlisting}
-               onSelect={() => handleSelectEntity(selectedForUnlisting.tokenId)}
-               borderType={borderType}
+              entity={selectedForUnlisting}
+              onSelect={() => handleSelectEntity(selectedForUnlisting.tokenId)}
+              borderType={borderType}
             />
-            </div>
-          <div className="w-6/12 flex flex-col justify-center items-center gap-3 pb-3 mt-5">
-            <Button
-              bg="#023340"
-              borderColor={buttonColor}
-              text="Unlist Entity"
-            />
-            <div className="w-8/12 md:w-7/12 lg:w-5/12 xl:w-3/12 justify-center mt-1">
-            <Button
-              bg="#023340"
-              borderColor={buttonColor}
-              text="Back"
-              onClick={() => setCurrentStep(2)}
-            />
-            </div>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-3 pb-3 mt-5">
+            {isLoading ? (
+              <LoadingSpinner color="#FF5F1F" />
+            ) : (
+              <>
+                <Button
+                  bg="#023340"
+                  borderColor={buttonColor}
+                  text="Unlist Entity"
+                  onClick={handleUnlist}
+                  disabled={isLoading}
+                />
+                <div className="justify-center mt-1">
+                  <Button
+                    bg="#023340"
+                    borderColor={buttonColor}
+                    text="Back"
+                    onClick={() => setCurrentStep(2)}
+                    disabled={isLoading}
+                  />
+                </div>
+              </>
+            )}
           </div>
         </div>
       )}
