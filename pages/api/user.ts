ec7ec172d53cbe773692db2c1dceb9553c33a754
atroboj <atroboj@gmail.com>
2024-07-11 17:49:36 +0800

    feat: create user apis

---
+++ pages/api/user.ts
@@ -0,0 +1,67 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '~/lib/prisma';
+
+const createUser = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { walletAddress } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: { walletAddress },
+    });
+    res.status(200).send({ user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+};
+
+const updateUser = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { walletAddress, name, twitter, pfp } = req.body;
+  try {
+    const user = await prisma.user.update({
+      where: { walletAddress },
+      data: {
+        walletAddress,
+        name,
+        twitter,
+        pfp,
+      },
+    });
+    res.status(200).send({ user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+};
+
+const getUser = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { walletAddress } = req.query;
+  try {
+    const user = await prisma.user.findFirst({
+      where: { walletAddress: walletAddress as string },
+    });
+    if (user) {
+      res.status(200).send({ user });
+    } else {
+      res.status(404).send({ error: 'User does not exist' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    await createUser(req, res);
+  } else if (req.method === 'PUT') {
+    await updateUser(req, res);
+  } else if (req.method === 'GET') {
+    await getUser(req, res);
+  } else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
