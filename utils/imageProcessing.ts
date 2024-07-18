85397d24375ccdf26666ac46194a62ac892135ad
atroboj <atroboj@gmail.com>
2024-07-18 08:19:21 +0800

    fix: resolve s3 file read

--- utils/imageProcessing.ts
+++ utils/imageProcessing.ts
@@ -292,23 +292,23 @@ const hexToRgb = (hex: string) => {
   return { r, g, b };
 };
 
-// const getS3Object = async (fileName: string) => {
-//   try {
-//     const params: S3.GetObjectRequest = {
-//       Bucket: process.env.AWS_S3_BUCKET_NAME || '',
-//       Key: `variables/${fileName}`,
-//     };
-
-//     const data = await s3.getObject(params).promise();
-
-//     return data.Body;
-//   } catch (e: any) {
-//     throw new Error(`Could not retrieve file from S3: ${e.message}`);
-//   }
-// };
-
-// Local method for faster processing
 const getS3Object = async (fileName: string) => {
-  const imagePath = path.join(varConfig.variablesPath, fileName);
-  return await sharp(imagePath).toBuffer();
+  try {
+    const params: S3.GetObjectRequest = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME || '',
+      Key: `variables/${fileName}`,
+    };
+
+    const data = await s3.getObject(params).promise();
+
+    return data.Body;
+  } catch (e: any) {
+    throw new Error(`Could not retrieve file from S3: ${e.message}`);
+  }
 };
+
+// // Local method for faster processing
+// const getS3Object = async (fileName: string) => {
+//   const imagePath = path.join(varConfig.variablesPath, fileName);
+//   return await sharp(imagePath).toBuffer();
+// };
