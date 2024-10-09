4d98982ed4245789447a10519e61f3452c5cb63e
TForge1 <traitforge@protonmail.com>
2024-10-09 16:10:01 +0930

    trading contract

--- constants/address.ts
+++ constants/address.ts
@@ -3,7 +3,7 @@ export const CONTRACT_ADDRESSES = {
   TraitForgeNft: '0xCabF9e769a9Dd211836A0e417f3f5fDF57EF2309' as `0x${string}`,
   EntropyGenerator:
     '0x31248a94C8ec87991834f175b1454D4e0E16D0A0' as `0x${string}`,
-  EntityTrading: '0x3a61c55bC64B34246A890F3aD5BabC4B74dAb777' as `0x${string}`,
+  EntityTrading: '0xD935c76577CE073f8260a0C7cb23662A7ac3eeCD' as `0x${string}`,
   EntityForging: '0xE1d5493b321d16e12c747bEc0E1ab4d4dBBf1AF9' as `0x${string}`,
   DevFund: '0x87F1a488229995c9c24C7C528440b21A946F36c1' as `0x${string}`,
   Airdrop: '0x8f1D2e2049A9FC72cE4CDA4eC3b920CBb108eb1B' as `0x${string}`,
